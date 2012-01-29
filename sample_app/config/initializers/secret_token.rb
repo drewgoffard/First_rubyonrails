0afa1f65fc678e897757372021cbbd05df59901b
Drew Goffard <drew.goffard@gmail.com>
2012-01-28 21:02:17 -0600

    sevarl fixes

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '56042617ab1116e444e4fdf44c7f8fe98c2af9e66b336d7d6f603238a13f4f6d85b33294a7d40ebda8fc79bad9002c9c039c4e6d4bf3d96aaf0aca582ccdc648'
